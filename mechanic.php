3ea6e028d190ca7643de661369e8b69affd992ed
bwoh <bwoh@outlook.kr>
2021-01-19 15:21:40 +0000

    apply css

--- mechanic.php
+++ mechanic.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="css/gunpla.css">
 		<title>Mechanic</title>
 		<body>
 			<table>
@@ -14,34 +15,34 @@
 					<th>height</th>
 					<th>weight</th>
 				</tr>
-			<?php
-				$dbc = mysqli_connect('localhost', 
-									  'root', 'mysql1234',
-									  'gunpladb')
-					or die('Error Connect DB');
+				<?php
+					$dbc = mysqli_connect('localhost', 
+										  'root', 'mysql1234',
+										  'gunpladb')
+						or die('Error Connect DB');
 
-				mysqli_query($dbc, 'set names utf8');
+					mysqli_query($dbc, 'set names utf8');
 
-				$query = 'select * from mechanic';
-				$result = mysqli_query($dbc, $query)
-					or die('Error Query');
+					$query = 'select * from mechanic';
+					$result = mysqli_query($dbc, $query)
+						or die('Error Query');
 
-				while ($row = mysqli_fetch_assoc($result)) {
-					echo '<tr class="mechanic">';
-					echo '<td>' . $row['id'] . '</td>';
-					echo '<td>' . $row['name'] . '</td>';
-					echo '<td>' . $row['model'] . '</td>';
-					echo '<td>' . $row['manufacturer'] . '</td>';
-					echo '<td>' . $row['armor'] . '</td>';
-					echo '<td>' . $row['height'] . '</td>';
-					echo '<td>' . $row['weight'] . '</td>';
-					echo '</tr>';
-				}
+					while ($row = mysqli_fetch_assoc($result)) {
+						echo '<tr class="mechanic">';
+						echo '<td>' . $row['id'] . '</td>';
+						echo '<td>' . $row['name'] . '</td>';
+						echo '<td>' . $row['model'] . '</td>';
+						echo '<td>' . $row['manufacturer'] . '</td>';
+						echo '<td>' . $row['armor'] . '</td>';
+						echo '<td>' . $row['height'] . '</td>';
+						echo '<td>' . $row['weight'] . '</td>';
+						echo '</tr>';
+					}
 
-				mysqli_free_result($result);
+					mysqli_free_result($result);
 
-				mysqli_close($dbc);
-			?>			
+					mysqli_close($dbc);
+				?>			
 				
 			</table>
 		</body>
