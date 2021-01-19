b3b18dfc8b8086246858d6571640e608a70833b1
bwoh <bwoh@outlook.kr>
2021-01-19 15:13:32 +0000

    update for table

--- mechanic.php
+++ mechanic.php
@@ -4,26 +4,46 @@
 		<meta charset="utf-8"/>
 		<title>Mechanic</title>
 		<body>
-		<?php
-			$dbc = mysqli_connect('localhost', 
-								  'root', 'mysql1234',
-								  'gunpladb')
-				or die('Error Connect DB');
-			
-			mysqli_query($dbc, 'set names utf8');
-			
-			$query = 'select * from mechanic';
-			$result = mysqli_query($dbc, $query)
-				or die('Error Query');
-			
-			while ($row = mysqli_fetch_assoc($result)) {
-				echo $row['name'] . '<br/>';
-			}
-			
-			mysqli_free_result($result);
+			<table>
+				<tr>
+					<th>id</th>
+					<th>name</th>
+					<th>model</th>
+					<th>manufacturer</th>
+					<th>armor</th>
+					<th>height</th>
+					<th>weight</th>
+				</tr>
+			<?php
+				$dbc = mysqli_connect('localhost', 
+									  'root', 'mysql1234',
+									  'gunpladb')
+					or die('Error Connect DB');
+
+				mysqli_query($dbc, 'set names utf8');
+
+				$query = 'select * from mechanic';
+				$result = mysqli_query($dbc, $query)
+					or die('Error Query');
+
+				while ($row = mysqli_fetch_assoc($result)) {
+					echo '<tr class="mechanic">';
+					echo '<td>' . $row['id'] . '</td>';
+					echo '<td>' . $row['name'] . '</td>';
+					echo '<td>' . $row['model'] . '</td>';
+					echo '<td>' . $row['manufacturer'] . '</td>';
+					echo '<td>' . $row['armor'] . '</td>';
+					echo '<td>' . $row['height'] . '</td>';
+					echo '<td>' . $row['weight'] . '</td>';
+					echo '</tr>';
+				}
+
+				mysqli_free_result($result);
+
+				mysqli_close($dbc);
+			?>			
 				
-			mysqli_close($dbc);
-		?>			
+			</table>
 		</body>
 	</head>
 </html>
