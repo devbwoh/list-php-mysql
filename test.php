db9d71a8fe1cd58f7c47f4b5c0babff0231e7401
bwoh <bwoh@outlook.kr>
2021-01-19 13:47:05 +0000

    add test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html>
+	<head>
+		<title>PHP 예제</title>
+		<meta charset="utf-8"/>
+		<body>
+		<?php
+			echo '반갑습니다.<br/>';
+			echo date('Y년 m월 d일 H시 i분 s초');
+		?>			
+		</body>
+	</head>
+</html>
